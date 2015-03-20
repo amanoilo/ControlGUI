a83be23e16a04eff825db64de71421587a073ae1
Alex Manoilo <amanoilo@mymail.mines.edu>
2015-03-19 19:02:30 -0600

    done. went for an aesthetic feel.

--- src/Control.java
+++ src/Control.java
@@ -9,41 +9,54 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.JComboBox;
+
 
 
 public class Control extends JFrame {
 
-	private JTextField Guess;
-    private JTextField Response;
-    private JTextField Die;
+	private JTextField guess;
+    private JTextField response;
+    private JTextField die;
+    private JTextField current;
     
     
     public Control()
     {
     	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	setTitle("Game Controls");
-    	setSize(400,400);
-    	JPanel panel = createTurnPanel();
-    	add(panel, BorderLayout.WEST);
+    	setSize(700,100);
+    	JPanel panel = createTurnAndDiePanel();
+    	add(panel, BorderLayout.CENTER);
     	
     	panel = createActionPanel();
     	add(panel, BorderLayout.WEST);
 
-    	panel = createDiePanel();
-    	add(panel, BorderLayout.EAST);
-
     	panel = createGuessPanel();
     	add(panel, BorderLayout.EAST);
-
-    	panel = createGuessResultPanel();
-    	add(panel, BorderLayout.EAST);    	
+  	
     }
     
-    private JPanel createTurnPanel()
+    private JPanel createTurnAndDiePanel()
     {
     	JPanel panel = new JPanel();
+    	die = new JTextField(5);
+    	JLabel dieText = new JLabel("Die Roll");
     	
-    	return panel;
+    	current = new JTextField(20);
+    	JLabel currentText = new JLabel("Current Player");
+    	
+    	die.setEditable(false);
+    	
+    	panel.setLayout( new GridLayout(2,0));
+    	panel.add(currentText);
+		panel.add(current);
+		
+		panel.add(dieText);
+		panel.add(die);
+		panel.setBorder(new TitledBorder (new EtchedBorder(), "Attribute box"));
+
+		return panel;
     }
     
     private JPanel createActionPanel()
@@ -55,34 +68,36 @@ public class Control extends JFrame {
     	panel.add(next);
     	panel.add(accuse);
     	
-		setLayout(new GridLayout(2, 1));
+		panel.setLayout(new GridLayout(2, 1));
 
     	return panel;
     }
     
-    private JPanel createAccusePanel()
-    {
-    	JPanel panel = new JPanel();
-    	return panel;
-    }
     
-    private JPanel createDiePanel()
-    {
-    	JPanel panel = new JPanel();
-    	return panel;
-    }
     
     private JPanel createGuessPanel()
     {
+    	
+    	guess = new JTextField(15);
+    	JLabel guessText = new JLabel("Guess");
+    	
+    	response = new JTextField(15);
+    	JLabel responseText = new JLabel("Guess Response");
+    	
+    	guess.setEditable(false);
+    	response.setEditable(false);
     	JPanel panel = new JPanel();
-    	return panel;
+    	panel.setLayout( new GridLayout(2,0));
+    	panel.add(guessText);
+		panel.add(guess);
+		
+		panel.add(responseText);
+		panel.add(response);
+		panel.setBorder(new TitledBorder (new EtchedBorder(), "Guess box"));
+
+		return panel;
     }
-    
-    private JPanel createGuessResultPanel()
-    {
-    	JPanel panel = new JPanel();
-    	return panel;
-    }
+
     
     public static void main(String[] args) {
 		Control gui = new Control();
